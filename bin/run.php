ebd29b1c8b8c49ade679ec7442d9689c1b4c6a4c
Sean Nessworthy <sean@nessworthy.me>
2018-03-31 13:45:03 +0100

    Added a few missing files.

---
+++ bin/run.php
@@ -0,0 +1,14 @@
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../src/auryn.php';
+
+$dotEnv = new Dotenv\Dotenv(__DIR__);
+$dotEnv->load();
+
+$injector = new Auryn\Injector();
+auryn_setup_dependencies($injector, $_ENV);
+
+/** @var \Nessworthy\Button\LoopRunner $loopRunner */
+$loopRunner = $injector->make(\Nessworthy\Button\LoopRunner::class);
+
+\Amp\Loop::run([$loopRunner, 'run']);
